82762a895a51ea95fafd512a3ff29c5d05cb139f
Akmaliin <akmalnafis25@gmail.com>
2024-05-22 23:49:27 +0700

    Adding Nav Button

---
+++ app/src/main/java/com/km6/flynow/presentation/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.km6.flynow.presentation.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    fun isLoggedIn(): Boolean {
+        return true
+    }
+}
